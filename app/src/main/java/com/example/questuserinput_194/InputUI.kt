2e1dd7df81627b1fce77394ca3599fc96c3cbd1b
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 12:12:15 +0700

    membuat variabel untuk menginilisiasikan value yang akan di masukan di form inputan

--- app/src/main/java/com/example/questuserinput_194/InputUI.kt
+++ app/src/main/java/com/example/questuserinput_194/InputUI.kt
@@ -1,10 +1,20 @@
 package com.example.questuserinput_194
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 
 fun FormInputan (modifier : Modifier){
+
+    var textNama by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+
 
 }
