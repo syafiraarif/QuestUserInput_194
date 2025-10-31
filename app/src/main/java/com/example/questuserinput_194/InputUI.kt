502caad084b22c175f24e97797a696dd71ab91cd
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 12:55:52 +0700

    membuat komponen button untuk submit inputan

--- app/src/main/java/com/example/questuserinput_194/InputUI.kt
+++ app/src/main/java/com/example/questuserinput_194/InputUI.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -144,9 +145,19 @@ fun FormInputan (modifier : Modifier){
 
                 Spacer(modifier = Modifier.height(24.dp))
 
-
+                Button(
+                    onClick = {
+                        nama = textNama
+                        jenis = textJK
+                        status = textStatus
+                        alamat = textAlamat
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = textNama.isNotEmpty() && textJK.isNotEmpty() && textStatus.isNotEmpty() && textAlamat.isNotEmpty()
+                ) {
+                    Text("Submit")
+                }
             }
         }
-
     }
 }
