0248a494137ad77ea3abc763894bb8d89b966718
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 10:08:28 +0700

    menambahkkan card yang memiliki bayangan untuk wadah hasil output data pengguna

--- app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -115,6 +118,14 @@ fun FormDataDiri(modifier: Modifier){
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+
+        ElevatedCard (
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(100.dp)
+                .width(300.dp )
+        ){  }
 
     }
 }
