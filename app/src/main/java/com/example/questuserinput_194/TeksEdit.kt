bf228a3a4317a5cd588141db1c1bd87edcd71353
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 09:36:32 +0700

    menambahkan komponen OutlinedTextField

--- app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
@@ -3,6 +3,10 @@ package com.example.questuserinput_194
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,7 +15,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
@@ -30,7 +33,18 @@ fun FormDataDiri(modifier: Modifier){
     Column (modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
 
+            label = {Text(text = "Nama Lengkap")},
+
+            onValueChange = {
+                textNama = it
+            }
+        )
     }
 
 }
