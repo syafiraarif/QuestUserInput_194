5efb784f4ffd79b0d7da200e4d43cbc3cb6db34f
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 09:43:37 +0700

    menambahkan komponen OutlinedTextFiled untuk dijadikan sebagai wadah inputan nantinya

--- app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
@@ -67,6 +67,17 @@ fun FormDataDiri(modifier: Modifier){
                 }
             }
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(250.dp),
+
+            label = {Text(text = "Alamat Lengkap")},
+
+            onValueChange = {
+                textAlamat = it
+            }
+        )
     }
 
 }
