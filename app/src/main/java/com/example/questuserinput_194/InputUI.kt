3e381c0aa44d0f98610ca94f0690951cd6c678d6
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 12:39:55 +0700

    membuat komponen card sebagai background atau sebagai wadah untuk variabel inputan

--- app/src/main/java/com/example/questuserinput_194/InputUI.kt
+++ app/src/main/java/com/example/questuserinput_194/InputUI.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,6 +62,12 @@ fun FormInputan (modifier : Modifier){
         }
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        ) {}
 
     }
 }
