49a20f4f5c5f7e676af84290954b9341a0247918
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 09:55:01 +0700

    menambahkan komponen buttom yang dimana dia nantinya bisa untuk button submit dan menyimpan data di variabel yang sudah di buat masing masing

--- app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
@@ -3,9 +3,11 @@ package com.example.questuserinput_194
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -20,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -89,5 +92,20 @@ fun FormDataDiri(modifier: Modifier){
             thickness = dimensionResource(R.dimen.padding_tipis),
             color = Color.DarkGray
         )
+
+        Button (
+            modifier = Modifier.fillMaxWidth(1f),
+            //the buttom is enabled when the user makes a selection
+            enabled = textAlamat.isNotEmpty(),
+
+            onClick = {
+                nama = textNama
+                jenis= textJK
+                alamat= textAlamat
+            }
+        ) {
+            Text(stringResource(R.string.submit))
+        }
+
     }
 }
