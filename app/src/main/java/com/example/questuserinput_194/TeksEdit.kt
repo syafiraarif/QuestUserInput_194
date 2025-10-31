10862e1742480c95e2aaafd326fc18ebd269d3e2
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 09:12:36 +0700

    menambahkan 3 variabel untuk nantinya di form input sebelum di submit

--- app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_194/TeksEdit.kt
@@ -1,9 +1,16 @@
 package com.example.questuserinput_194
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
-
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 }
