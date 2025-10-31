bac12e0ae70bc7efd406a5643c278012bd78e032
syairaarif <putrisyafira4444@gmail.com>
2025-10-31 12:09:23 +0700

    membuat fungsi FormInputan

--- app/src/main/java/com/example/questuserinput_194/InputUI.kt
+++ app/src/main/java/com/example/questuserinput_194/InputUI.kt
@@ -1,2 +1,10 @@
 package com.example.questuserinput_194
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun FormInputan (modifier : Modifier){
+
+}
